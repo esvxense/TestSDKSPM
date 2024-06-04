e143112592be7c83314f243a09cf261720d5849f
esvxense <esvxense@gmail.com>
2024-06-04 17:44:09 +0300

    init commit

---
+++ Sources/SimpleMath.xcframework/ios-arm64_x86_64-simulator/SimpleMath.framework/Headers/SimpleMath.h
@@ -0,0 +1,18 @@
+//
+//  SimpleMath.h
+//  SimpleMath
+//
+//  Created by Alexander Snitko on 4.06.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleMath.
+FOUNDATION_EXPORT double SimpleMathVersionNumber;
+
+//! Project version string for SimpleMath.
+FOUNDATION_EXPORT const unsigned char SimpleMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleMath/PublicHeader.h>
+
+
